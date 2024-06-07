891376008ab84674e9587a3e193773109807b5e0
prezrenz <perezrenjie20@gmail.com>
2024-06-07 14:23:19 +0800

    implement user class

---
+++ src/main/java/com/cs1e/User.java
@@ -0,0 +1,101 @@
+package com.cs1e;
+
+public class User {
+    String name;
+    String email;
+    String password;
+    boolean isAdmin;
+
+    String address;
+    int previousReading;
+    int currentReading;
+    int consumption;
+    int totalDue;
+    String dueDate;
+    String status;
+
+    String creditCardNumber;
+    int balance;
+
+    User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = true;
+
+        address = "";
+        previousReading = 0;
+        currentReading = 0;
+        consumption = 0;
+        totalDue = 0;
+        dueDate = "";
+        status = "admin";
+
+        creditCardNumber = "";
+        balance = 0;
+    }
+
+    User(String name, String email, String password, String address, 
+            String creditCardNumber, int balance) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = false;
+
+        this.address = address;
+        this.previousReading = 0;
+        this.currentReading = 0;
+        this.consumption = 0;
+        this.totalDue = 0;
+        this.dueDate = "";
+        this.status = "UNVERIFIED";
+
+        this.creditCardNumber = creditCardNumber;
+        this.balance = 0;
+    }
+
+    User(String name, String email, String password, boolean isAdmin, String address,
+            int previousReading, int currentReading, int consumption, int totalDue,
+            String dueDate, String status, String creditCardNumber, int balance) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = false;
+
+        this.address = address;
+        this.previousReading = previousReading;
+        this.currentReading = currentReading;
+        this.consumption = consumption;
+        this.totalDue = totalDue;
+        this.dueDate = dueDate;
+        this.status = status;
+
+        this.creditCardNumber = creditCardNumber;
+        this.balance = balance;
+    }
+
+    Object[] tabularize() {
+        Object[] data = {this.name, this.email, this.address,
+                            this. previousReading, this.currentReading, this.consumption,
+                            this.totalDue, this.dueDate, this.status};
+
+        return data;
+    }
+
+    String stringify() {
+        return String.format("%s,%s,%s,%b,%s,%d,%d,%d,%d,%s,%s,%d,%d",
+                            name, email, password, isAdmin,
+                            address, previousReading, currentReading, consumption,
+                            totalDue, dueDate, status);
+    }
+
+    void setNewReading(int newReading, String newDueDate, final int RATE) {
+        previousReading = currentReading;
+        currentReading = newReading;
+        consumption = currentReading - previousReading;
+        totalDue = consumption * RATE;
+
+        dueDate = newDueDate;
+        status = "UNPAID";
+    }
+}
