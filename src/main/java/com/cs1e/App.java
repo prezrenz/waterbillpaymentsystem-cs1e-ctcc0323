b1518414c2941d2daa8e550d7ee8ba80d9a24fb8
prezrenz <perezrenjie20@gmail.com>
2024-06-15 09:29:16 +0800

    set app size to 540x400 and center frame

--- src/main/java/com/cs1e/App.java
+++ src/main/java/com/cs1e/App.java
@@ -21,7 +21,7 @@ public class App extends JFrame
     Login loginScreen;
     Registration registrationScreen;
 
-    App() {
+    App(final int WIDTH, final int HEIGHT) {
         database = new Database();
 
         cardLayout = new CardLayout();
@@ -44,11 +44,15 @@ public class App extends JFrame
         });
 
         setLayout(new FlowLayout());
-        setSize(960, 540);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null); // centers the frame
         setVisible(true);
     }
 
     public static void main( String[] args ) {
-        new App();
+        final int WIDTH = 540;
+        final int HEIGHT = 400;
+
+        new App(WIDTH, HEIGHT);
     }
 }
